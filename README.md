ee1a3a8abad2d246f6b3802d3bb84e9cf279b465
Per Enström <enstrom.per@gmail.com>
2020-02-15 16:51:40 +0100

    Add babel, heroku, eslint, etc.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pers-boilerplate
 A boilerplate express web server
+
+1. Clone the repo
+1. Run `rm -rf .git` to remove the git repo
+1. Set up a new git repo
+1. Run `heroku create [your-app-name]`
+1. Set up the pipeline in Heroku
---
+++ build/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+var port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+  return res.send('Hello World!');
+});
+app.listen(port, function () {
+  return console.log("App listening on port ".concat(port, "!"));
+});
+//# sourceMappingURL=app.js.map
